e83736b6ca8cf7977a426e272b5aaef6ff2023bc
lgao1726 <LingfanGao@hotmail.com>
2015-12-08 00:38:24 +0100

    Update syscall.h

--- sysif/src/syscall.h
+++ sysif/src/syscall.h
@@ -1,8 +1,9 @@
-#ifndef SYSCALL_H
-#define SYSCALL_H
+#ifndef SYSCALL_H_INCLUDED
+#define SYSCALL_H_INCLUDED
 
 #include "util.h"
 #include "hw.h"
+#include "asm_tools.h"
 #include <stdint.h>
 #include "sched.h"
 void sys_reboot();
@@ -11,10 +12,7 @@ void sys_nop(void);
 void sys_settime(uint64_t date_ms);
 uint64_t sys_gettime(void);
 void sys_yieldto(struct pcb_s* dest);
-void do_sys_yieldto();
 void sys_yield();
-void do_sys_yield();
+uint32_t sys_exit();
 
 #endif
-
-
