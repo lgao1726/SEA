cd5441c8e0bf6dd8f0cd3c2eeeac02780fffc3ad
lgao1726 <LingfanGao@hotmail.com>
2015-12-08 14:57:42 +0100

    Update syscall.c

--- sysif/src/syscall.c
+++ sysif/src/syscall.c
@@ -79,36 +79,6 @@ void do_sys_gettime(void)
 	return;
 }
 
-
-void copyRegistres(){
-	__asm("mov %0,r0" : "=r"(current_process->registres[0]));
-	__asm("mov %0,r1" : "=r"(current_process->registres[1]));
-	__asm("mov %0,r2" : "=r"(current_process->registres[2]));
-	__asm("mov %0,r3" : "=r"(current_process->registres[3]));
-	__asm("mov %0,r4" : "=r"(current_process->registres[4]));
-	__asm("mov %0,r5" : "=r"(current_process->registres[5]));
-	__asm("mov %0,r6" : "=r"(current_process->registres[6]));
-	__asm("mov %0,r7" : "=r"(current_process->registres[7]));
-	__asm("mov %0,r8" : "=r"(current_process->registres[8]));
-	__asm("mov %0,r9" : "=r"(current_process->registres[9]));
-	__asm("mov %0,r10" : "=r"(current_process->registres[10]));
-	__asm("mov %0,r11" : "=r"(current_process->registres[11]));
-	__asm("mov %0,r12" : "=r"(current_process->registres[12]));	
-	
-	__asm("mov %0,lr" : "=r"(current_process->lr_svc));
-	//save cpsr_user by copying spsr_svc
-	__asm("mrs r0,spsr");
-	__asm("mov %0,r0" : "=r"(current_process->cpsr));
-	//we can't access the user LR register from SVC
-	//switch temporarily to System mode to load LR
-	__asm("cps 0x1F");
-	__asm("mov %0,lr" :"=r"(current_process->lr_user));
-	__asm("mov %0,sp" :"=r"(current_process->sp));
-	__asm("cps 0x13");
-
-
-	
-}
 /*dispatcher that schedules processes*/
 void sys_yield(){
 	int num = 6;	
@@ -117,30 +87,6 @@ void sys_yield(){
 
 }
 
-void do_sys_yield(){
-	copyRegistres();
-	elect();
-	//restore context
-	__asm("mov r0, %0" : :"r"(current_process->registres[0]));
-	__asm("mov r1, %0" : :"r"(current_process->registres[1]));
-	__asm("mov r2, %0" : :"r"(current_process->registres[2]));
-	__asm("mov r3, %0" : :"r"(current_process->registres[3]));
-	__asm("mov r4, %0" : :"r"(current_process->registres[4]));
-	__asm("mov r5, %0" : :"r"(current_process->registres[5]));
-	__asm("mov r6, %0" : :"r"(current_process->registres[6]));
-	__asm("mov r7, %0" : :"r"(current_process->registres[7]));
-	__asm("mov r8, %0" : :"r"(current_process->registres[8]));
-	__asm("mov r9, %0" : :"r"(current_process->registres[9]));
-	__asm("mov r10, %0" : :"r"(current_process->registres[10]));
-	__asm("mov r11, %0" : :"r"(current_process->registres[11]));
-	__asm("mov r12, %0" : :"r"(current_process->registres[12]));
-	//we can't access the user LR register from SVC
-	//switch temporarily to System mode to load LR
-	__asm("cps 0x1F");
-	__asm("mov lr, %0" : :"r"(current_process->lr_user));
-	__asm("mov sp, %0" : :"r"(current_process->sp));
-	__asm("cps 0x13");
-}
 
 /*passe la main a un autre processus*/
 void sys_yieldto(struct pcb_s* dest){
@@ -151,32 +97,9 @@ void sys_yieldto(struct pcb_s* dest){
 }
 
 void do_sys_yieldto(){
-	struct pcb_s* dest;
- 	copyRegistres(); 
+	context_save_to_pcb();
 	elect();
-	//restore context
-	__asm("mov r0, %0" : :"r"(current_process->registres[0]));
-	__asm("mov r1, %0" : :"r"(current_process->registres[1]));
-	__asm("mov r2, %0" : :"r"(current_process->registres[2]));
-	__asm("mov r3, %0" : :"r"(current_process->registres[3]));
-	__asm("mov r4, %0" : :"r"(current_process->registres[4]));
-	__asm("mov r5, %0" : :"r"(current_process->registres[5]));
-	__asm("mov r6, %0" : :"r"(current_process->registres[6]));
-	__asm("mov r7, %0" : :"r"(current_process->registres[7]));
-	__asm("mov r8, %0" : :"r"(current_process->registres[8]));
-	__asm("mov r9, %0" : :"r"(current_process->registres[9]));
-	__asm("mov r10, %0" : :"r"(current_process->registres[10]));
-	__asm("mov r11, %0" : :"r"(current_process->registres[11]));
-	__asm("mov r12, %0" : :"r"(current_process->registres[12]));
-	//we can't access the user LR register from SVC
-	//switch temporarily to System mode to load LR
-	__asm("cps 0x1F");
-	__asm("mov lr, %0" : :"r"(current_process->lr_user));
-	__asm("mov sp, %0" : :"r"(current_process->sp));
-	__asm("cps 0x13");
-
-	
-
+	context_load_from_pcb();
 }
 
 //terminate the process  
@@ -190,7 +113,7 @@ uint32_t sys_exit(){
 void do_sys_exit(){
 	struct pcb_s* to_delete = current_process;
 	to_delete->status = PROCESS_TERMINATED;
-	do_sys_yield();
+	do_sys_yieldto();
 	int terminated = 0;
 	if(to_delete==current_process){
 		terminated = 1;
@@ -211,7 +134,8 @@ void __attribute__((naked)) swi_handler(void)
 {
 	
 	
-	__asm("stmfd sp!,{r0-r12,lr}");	
+	__asm("stmfd sp!,{r0-r12,lr}");
+	__asm("mov %0,sp" : "=r"(stack));	
 	int num;
 	__asm("mov %0,r0" : "=r"(num));
 	if(num == 1){
@@ -219,10 +143,8 @@ void __attribute__((naked)) swi_handler(void)
 	}else if(num == 2){
 		do_sys_nop();
 	}else if(num == 3){
-		__asm("mov %0,sp" : "=r"(stack));
 		do_sys_settime();
 	}else if(num == 4){
-		__asm("mov %0,sp" : "=r"(stack));
 		do_sys_gettime();
 	}else if(num == 5){
 		do_sys_yieldto();	
